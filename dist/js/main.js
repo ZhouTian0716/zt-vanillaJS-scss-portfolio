dac34d2c51c0a1a4e49e5799195a0e167391b9d4
ZhouTian0716 <egglord0716@gmail.com>
2022-12-17 01:31:15 +1100

    working on navbar mobile

--- dist/js/main.js
+++ dist/js/main.js
@@ -1,1 +1,15 @@
-console.log('hi')
+let lastScrollTop;
+navbar = document.querySelector("#nav_bar");
+
+window.addEventListener("scroll", function () {
+  let scrollTop = window.pageYOffset || document.documentElement.scrollTop;
+
+  if (scrollTop > lastScrollTop) {
+    navbar.style.top = "-80px";
+  } else if (scrollTop === 0) {
+    navbar.style.top = "0px";
+  } else {
+    navbar.style.top = "-10px";
+  }
+  lastScrollTop = scrollTop;
+});
