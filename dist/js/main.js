1fffdb4c48c61bba855f8ec9793a3fbe5dc70949
ZhouTian0716 <egglord0716@gmail.com>
2022-12-19 20:11:28 +1100

    work experience section animation done

--- dist/js/main.js
+++ dist/js/main.js
@@ -18,30 +18,39 @@ window.addEventListener("scroll", function () {
 let expIndex;
 let deviceWidth;
 const expBtns = document.querySelectorAll(".experience__btn");
+const expDetails = document.querySelectorAll(".experience__details");
 const expTabs = document.querySelector(".experience__tabs");
 const expActive = document.querySelector("#experience__active");
 
 expBtns.forEach((e) =>
   e.addEventListener("click", (e) => {
     expIndex = parseInt(e.target.getAttribute("data-index"));
+    console.log(expIndex);
     deviceWidth = window.visualViewport.width;
+    // check translate X or Y depend on screen size
     if (deviceWidth < 768) {
-      // check translate X or Y depend on screen size
-      // expActive.style.transform = `translate(-100%, ${expIndex * 100}%)`;
-      expActive.style.transform = `translate( ${expIndex * 120}px,-2px )`;
+      expActive.style.transform = `translate( ${expIndex * 120}px,0px )`;
     } else {
       expActive.style.transform = `translate(-100%, ${expIndex * 100}%)`;
     }
+    // step 1: remove all active class
+    expDetails.forEach((el) => el.classList.remove("active"));
+    // step 2: add active class to selected one
+    expDetails.forEach((el) => {
+      if (parseInt(el.getAttribute("data-index")) === expIndex) {
+        el.classList.add("active");
+      }
+    });
   })
 );
 
+// This is only for develepment resizing
 window.addEventListener("resize", () => {
   deviceWidth = window.visualViewport.width;
   if (deviceWidth < 768) {
-    expActive.style.transform = `translate( ${expIndex * 120}px,-2px )`;
+    expActive.style.transform = `translate( ${expIndex * 120}px,0px )`;
   } else {
     expActive.style.transform = `translate( -100%, ${expIndex * 100}% )`;
   }
-
-  // console.log(deviceWidth);
 });
+// This is only for develepment resizing
